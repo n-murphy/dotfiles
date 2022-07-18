65f58ab392b0344cf658d3ae54c6be790148ceb2
n-murphy <38255714+n-murphy@users.noreply.github.com>
2022-07-18 10:30:13 +0100

    kicking the tires with homebrew on ubuntu

---
+++ setup_homebrew.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+echo "\n<<<Starting Homebrew Setup>>>\n"
+
+if exists brew; then
+  echo "brew exists, skipping install . . ."
+else
+  echo "brew doesn't exist, continuing with the install . . ."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
+# brew bundle --verbose
+
+
+# This works to solve the Insecure Directories issue:
+# From the Homebrew site for shell completions:
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
+chmod -R go-w "$(brew --prefix)/share"
