bb35092f704670b12ce31cca722b467c955aebee
n-murphy <38255714+n-murphy@users.noreply.github.com>
2022-07-19 13:25:00 +0100

    fix for vscode extension installation.

--- setup_vscode.zsh
+++ setup_vscode.zsh
@@ -6,14 +6,13 @@ echo ""
 VSCODE_SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 
 
-
 if [[ -d "$VSCODE_SETTINGS_DIR" ]] ; then 
   echo "Applying custom settings.json to $VSCODE_SETTINGS_DIR"
   cp ./settings.json $VSCODE_SETTINGS_DIR
+  
+  echo "Installing VS Code Extensions"
+  cat ./vscode_extensions | xargs -L 1 code --force --install-extension
 else
-  echo "$VSCODE_SETTINGS_DIR directory not found, exiting disgracefully"
-  exit 1
+  echo "$VSCODE_SETTINGS_DIR directory not found, exiting script"
+  exit 0
 fi
-
-echo "Installing VS Code Extensions"
-cat ./vscode_extensions | xargs -L 1 code --install-extension
